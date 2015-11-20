32b1945d8d5d9bacf88d218c4a301ae31ecc5909
Juan Pablo Marzetti <yonpols@gmail.com>
2015-11-19 20:01:11 -0800

    Initial Controller

---
+++ RouteToDo/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  RouteToDo
+//
+//  Created by Juan Pablo Marzetti on 11/19/15.
+//  Copyright © 2015 RouteToDo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
