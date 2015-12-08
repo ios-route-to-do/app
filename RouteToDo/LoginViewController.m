6b3201c48402c97699e90f584936c3e01573365a
Juan Pablo Marzetti <jmarzetti@groupon.com>
2015-12-08 14:35:32 -0800

    Login Screen

--- RouteToDo/LoginViewController.m
+++ RouteToDo/LoginViewController.m
@@ -19,6 +19,9 @@
 @property (weak, nonatomic) IBOutlet UITextField *emailTextField;
 @property (weak, nonatomic) IBOutlet UIButton *loginButton;
 
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *logoTopConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *emailTextFieldTopConstraint;
+
 @end
 
 @implementation LoginViewController
@@ -28,6 +31,33 @@
     self.loginButton.layer.cornerRadius = 5;
     self.loginButton.layer.masksToBounds = YES;
 }
+
+- (void)viewWillAppear:(BOOL)animated {
+    if (animated) {
+        self.logoTopConstraint.constant = 40;
+        self.emailTextField.alpha = 1;
+        self.loginButton.alpha = 1;
+    } else {
+        self.logoTopConstraint.constant = 179;
+        self.emailTextField.alpha = 0;
+        self.loginButton.alpha = 0;
+    }
+
+    [self.view layoutIfNeeded];
+}
+
+- (void) viewDidAppear:(BOOL)animated {
+    if (!animated) {
+        [UIView animateWithDuration:0.3 animations:^{
+            self.logoTopConstraint.constant = 40;
+            self.emailTextField.alpha = 1;
+            self.loginButton.alpha = 1;
+
+            [self.view layoutIfNeeded];
+        }];
+    }
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
